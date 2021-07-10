8400dac60828639178d34cd092e8997e059d2d78
LordCam2005 <cameronAcampbell@icloud.com>
2021-07-10 15:16:50 +1200

    fixing maps problem (deleted everything and started again)

---
+++ assets/maps/float.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="float" tilewidth="16" tileheight="16" tilecount="9" columns="3">
+ <image source="float.png" trans="ff00ff" width="48" height="48"/>
+</tileset>
---
+++ assets/maps/ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="ground" tilewidth="16" tileheight="16" tilecount="49" columns="7">
+ <image source="tileset jungle.png" trans="ff00ff" width="112" height="112"/>
+</tileset>
