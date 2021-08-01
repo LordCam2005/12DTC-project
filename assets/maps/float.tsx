0391d9a1f0f40abe58762479729ff4c5aa0928c2
LordCam2005 <cameronAcampbell@icloud.com>
2021-08-02 10:53:14 +1200

    fixed float scaling

--- assets/maps/float.tsx
+++ assets/maps/float.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="float" tilewidth="16" tileheight="16" tilecount="9" columns="3">
- <image source="float.png" trans="ff00ff" width="48" height="48"/>
+<tileset version="1.5" tiledversion="1.7.0" name="float.png" tilewidth="48" tileheight="48" tilecount="9" columns="3">
+ <image source="float.png" trans="ff00ff" width="144" height="144"/>
 </tileset>
