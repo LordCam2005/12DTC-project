3b1de180f7f5ca6e1a33752ebc1d55f1dd117399
LordCam2005 <cameronAcampbell@icloud.com>
2021-08-12 16:50:50 +1200

    checkpoints + finish point

---
+++ assets/maps/checkpoint.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="checkpoint" tilewidth="48" tileheight="96" tilecount="2" columns="2">
+ <image source="checkpoint.png" trans="ff00ff" width="96" height="96"/>
+</tileset>
---
+++ assets/maps/finish.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="finish" tilewidth="48" tileheight="96" tilecount="2" columns="2">
+ <image source="finish.png" trans="ff00ff" width="96" height="96"/>
+</tileset>
